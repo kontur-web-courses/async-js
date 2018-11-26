cd88da1073301be65843b789c8e0993a261ba8c8
aminopyridin <aminopyrodin@gmail.com>
2018-11-26 05:43:15 +0500

    add errors =)

---
+++ tasks/async/.solved/focus_part3_errors.js
@@ -0,0 +1,122 @@
+const API = {
+    organizationList: '/orgsList',
+    analytics: '/api3/analitics',
+    orgReqs: '/api3/reqBase',
+    buhForms: '/api3/buh',
+};
+
+async function run () {
+    try {
+        const orgOgrns = await sendRequest(API.organizationList);
+        const requisites = await sendRequest(`${API.orgReqs}?ogrn=${orgOgrns.join(',')}`);
+        const orgsMap = reqsToMap(requisites);
+
+        const analytics = await sendRequest(`${API.analytics}?ogrn=${orgOgrns.join(',')}`);
+        addInOrgsMap(orgsMap, analytics, 'analytics');
+
+        const buh = await sendRequest(`${API.buhForms}?ogrn=${orgOgrns.join(',')}`);
+        addInOrgsMap(orgsMap, buh, 'buhForms');
+        render(orgsMap, orgOgrns);
+    } catch (e) {
+        /* do nothing*/
+    }
+
+}
+run();
+
+async function sendRequest (url) {
+    return fetch(url).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert(response.status + ' ' + response.statusText);
+            return Promise.reject();
+        }
+    });
+}
+
+function reqsToMap (requisites) {
+    return requisites.reduce((acc, item) => {
+        acc[item.ogrn] = item;
+        return acc;
+    }, {});
+}
+
+function addInOrgsMap (orgsMap, additionalInfo, key) {
+    for (const item of additionalInfo) {
+        orgsMap[item.ogrn][key] = item[key];
+    }
+}
+
+function render (organizationsInfo, organizationsOrder) {
+    const table = document.getElementById('organizations');
+    table.classList.remove('hide');
+
+    const template = document.getElementById('orgTemplate');
+    const container = table.querySelector('tbody');
+
+
+    organizationsOrder.forEach(item => {
+        renderOrganization(organizationsInfo[item], template, container);
+    });
+}
+
+function renderOrganization (orgInfo, template, container) {
+    const clone = document.importNode(template.content, true);
+    const name = clone.querySelector('.name');
+    const indebtedness = clone.querySelector('.indebtedness');
+    const money = clone.querySelector('.money');
+    const address = clone.querySelector('.address');
+
+    name.textContent = (orgInfo.UL && orgInfo.UL.legalName && orgInfo.UL.legalName.short) || '';
+    indebtedness.textContent = formatMoney(orgInfo.analytics.s1002 || 0);
+
+    if (orgInfo.buhForms && orgInfo.buhForms.length && orgInfo.buhForms[orgInfo.buhForms.length -1] &&
+        orgInfo.buhForms[orgInfo.buhForms.length -1].year === 2017) {
+        money.textContent = formatMoney((orgInfo.buhForms[orgInfo.buhForms.length -1].form2 &&
+            orgInfo.buhForms[orgInfo.buhForms.length -1].form2[0] &&
+            orgInfo.buhForms[orgInfo.buhForms.length -1].form2[0].endValue) || 0);
+    } else {
+        money.textContent = '—';
+    }
+
+    const addressFromServer = orgInfo.UL.legalAddress.parsedAddressRF;
+    address.textContent = createAddress(addressFromServer);
+
+    container.appendChild(clone);
+}
+
+function formatMoney (money) {
+    let formatted = money.toFixed(2);
+    formatted = formatted.replace('.', ',');
+
+    const rounded = money.toFixed(0);
+    const numLen = rounded.length;
+    for (let i = numLen - 3; i > 0; i -= 3) {
+        formatted = `${formatted.slice(0, i)} ${formatted.slice(i)}`;
+    }
+
+    return `${formatted} ₽`;
+}
+
+function createAddress (address) {
+    const addressToRender = [];
+    if (address.regionName) {
+        addressToRender.push(createAddressItem('regionName'));
+    }
+    if (address.city) {
+        addressToRender.push(createAddressItem('city'));
+    }
+    if (address.street) {
+        addressToRender.push(createAddressItem('street'));
+    }
+    if (address.house) {
+        addressToRender.push(createAddressItem('house'));
+    }
+
+    return addressToRender.join(', ');
+
+    function createAddressItem (key) {
+        return `${address[key].topoShortName}. ${address[key].topoValue}`;
+    }
+}
